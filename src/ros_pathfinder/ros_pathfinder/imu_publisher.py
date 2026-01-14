d4608693919562e25bd1ad9f09a094f9dd5b256d
cliff42 <chris.cliff@shaw.ca>
2026-01-13 20:55:07 -0500

    switch to smbus

--- src/ros_pathfinder/ros_pathfinder/imu_publisher.py
+++ src/ros_pathfinder/ros_pathfinder/imu_publisher.py
@@ -3,15 +3,17 @@ from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 
 from std_msgs.msg import String
-import busio
-import board
+from smbus2 import SMBus, i2c_msg
 
 as5600 = 0x36
 icm29408 = 0x69
 bno085 = 0x4a
 
-i2c_1 = busio.I2C(board.D1, board.D0)
-# i2c_2 = busio.I2C(board.D1, board.D0)
+bus1 = SMBus(1)   # /dev/i2c-1
+bus2 = SMBus(2)   # /dev/i2c-2
+
+REG_STATUS = 0x0B
+REG_RAW_ANGLE_H = 0x0C  # read 2 bytes: 0x0C,0x0D
 
 class IMUPublisher(Node):
 
@@ -24,32 +26,18 @@ class IMUPublisher(Node):
 
     def timer_callback(self):
         msg = String()
-        msg.data = 'iteration: %d, status (1): %d, raw_angle (1): %d, status (2): %d, raw_angle (2): %d' % (self.i, self.get_status(i2c_1), self.get_raw_angle(i2c_1), self.get_status(i2c_1), self.get_raw_angle(i2c_1))
+        msg.data = 'iteration: %d, status (1): %d, raw_angle (1): %d, status (2): %d, raw_angle (2): %d' % (self.i, self.get_status(bus1), self.get_raw_angle(bus1), self.get_status(bus2), self.get_raw_angle(bus2))
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
 
-    def get_raw_angle(self, i2c):
-        i2c.writeto(as5600, bytes([0x0C]))
-        result = bytearray(2)
-        i2c.readfrom_into(as5600, result)
-        raw_angle = (result[0] << 8) | result[1]   # combine into 16-bit value
-        raw_angle &= 0x0FFF                    # mask out 12-bit angle (0–4095)
-        print("Raw angle:", (raw_angle * 360.0) / 4096.0) # convert to angle
-        return (raw_angle * 360.0) / 4096.0
+    def get_raw_angle(self, bus):
+        data = bus.read_i2c_block_data(as5600, REG_RAW_ANGLE_H, 2)
+        raw = ((data[0] << 8) | data[1]) & 0x0FFF
+        return (raw * 360.0) / 4096.0
     
-    def get_status(self, i2c):
-        i2c.writeto(as5600, bytes([0x0B]))
-        result = bytearray(1)
-        i2c.readfrom_into(as5600, result)
-        status = result[0]
-        print("as5600 status:", status)
-        bit3_set = bool(status & (1 << 3))
-        print("bit 3 set? (too strong)", bit3_set)
-        bit4_set = bool(status & (1 << 4))
-        print("bit 4 set? (too weak)", bit4_set)
-        bit5_set = bool(status & (1 << 5))
-        print("bit 5 set? (magnet detected)", bit5_set)
+    def get_status(self, bus):
+        status = bus.read_byte_data(as5600, REG_STATUS)
         return status
 
 
