1edfa772cea4e38c126d4da57dc55f3fe780f50b
cliff42 <chris.cliff@shaw.ca>
2026-01-21 22:49:50 -0500

    Made changes to imu_publisher and test_subscriber files. added function to get velocity for encoder. added imu data in addition to encoder data.

--- src/ros_pathfinder/ros_pathfinder/imu_publisher.py
+++ src/ros_pathfinder/ros_pathfinder/imu_publisher.py
@@ -4,6 +4,24 @@ from rclpy.node import Node
 
 from std_msgs.msg import String, Float64MultiArray
 from smbus2 import SMBus, i2c_msg
+import board
+import busio
+
+
+from adafruit_bno08x import (
+    BNO_REPORT_ACCELEROMETER,
+    BNO_REPORT_GYROSCOPE,
+    BNO_REPORT_MAGNETOMETER,
+    BNO_REPORT_ROTATION_VECTOR,
+)
+from adafruit_bno08x.i2c import BNO08X_I2C
+i2c = busio.I2C(board.SCL, board.SDA, frequency=400_000)
+bno = BNO08X_I2C(i2c, address=0x4a)
+bno.enable_feature(BNO_REPORT_ACCELEROMETER)
+bno.enable_feature(BNO_REPORT_GYROSCOPE)
+bno.enable_feature(BNO_REPORT_MAGNETOMETER)
+
+
 
 as5600 = 0x36
 icm29408 = 0x69
@@ -12,23 +30,31 @@ bno085 = 0x4a
 bus1 = SMBus(1)   # /dev/i2c-1
 bus2 = SMBus(2)   # /dev/i2c-2
 
+
+
 REG_STATUS = 0x0B
 REG_RAW_ANGLE_H = 0x0C  # read 2 bytes: 0x0C,0x0D
 
-class IMUPublisher(Node):
+class EncoderPublisher(Node):
 
     def __init__(self):
         super().__init__('imu_publisher')
         self.publisher_ = self.create_publisher(Float64MultiArray, 'test_topic', 10)
-        timer_period = 0.05  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.publisher2_ = self.create_publisher(Float64MultiArray,'imu_topic',10)
+        self.timer_period = 0.05  # seconds
+        self.timer = self.create_timer(self.timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
         msg = Float64MultiArray()
-        msg.data = [self.get_raw_angle(bus1), self.get_raw_angle(bus2)]
+        msg.data = [self.get_raw_angle(bus1), self.get_raw_angle(bus2),self.timer_period]
+        msg2 = Float64MultiArray()
+        msg2.data = [bno.acceleration[0],bno.acceleration[1],bno.acceleration[2],bno.gyro[0],bno.gyro[1],bno.gyro[2],self.timer_period]
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info('Publishing Encoder Data: "%s"' % msg.data)
+        self.publisher2_.publish(msg2)
+        self.get_logger().info('Publishing IMU Data: "%s"' % msg2.data)
+
         self.i += 1
 
     def get_raw_angle(self, bus):
@@ -44,9 +70,8 @@ class IMUPublisher(Node):
 def main(args=None):
     try:
         with rclpy.init(args=args):
-            imu_publisher = IMUPublisher()
-
-            rclpy.spin(imu_publisher)
+            encoder_publisher = EncoderPublisher()
+            rclpy.spin(encoder_publisher)
     except (KeyboardInterrupt, ExternalShutdownException):
         pass
 
--- src/ros_pathfinder/ros_pathfinder/test_subscriber.py
+++ src/ros_pathfinder/ros_pathfinder/test_subscriber.py
@@ -2,36 +2,101 @@ import rclpy
 from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 
+
+
+import math
+
 from std_msgs.msg import Float64MultiArray
 
 class MinimalSubscriber(Node):
-    distance_1 = 0
-    delta_angle = 0
-    prev_angle = 0
+    
+    
+    prev_angle_l = 0
+    prev_angle_r = 0
+    distance_l = 0
+    distance_r = 0
+    prev_distance_l = 0
+    prev_distance_r = 0
+    vel_l = 0
+    vel_r = 0
+    prev_vel_l = 0
+    prev_vel_r = 0
     count = 0
+
+    # vel_x, vel_y, vel_z = 0
+    # dist_x, dist_y, dist_z = 0
 
     def __init__(self):
         super().__init__('minimal_subscriber')
-        self.subscription = self.create_subscription(
-            Float64MultiArray,
-            'test_topic',
-            self.listener_callback,
-            10)
+        self.subscription = self.create_subscription(Float64MultiArray,'test_topic',self.listener_callback,10)
+        self.subscription2 = self.create_subscription(Float64MultiArray,'imu_topic',self.listener_callback2,10)
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        angle = int(msg.data[0])
-        self.get_logger().info('angle (2): "%s"' % msg.data[1])
-        if self.prev_angle > 270 and angle < 90:
-            self.delta_angle = angle + 360 - self.prev_angle
-        elif self.prev_angle < 90 and angle > 270:
-            self.delta_angle = -1 * (angle + 360 - self.prev_angle)
-        else:
-            self.delta_angle = angle - self.prev_angle
+        angle_l = float(msg.data[0])
+        angle_r = float(msg.data[1])
+        timer_period = float(msg.data[2])
+        time_elapsed = timer_period * self.count
+        # self.get_logger().info('angle (l): "%s"' % angle_l)
+        # self.get_logger().info('angle (r): "%s"' % angle_r)
+        # self.get_logger().info('timer period (s): "%s"' % timer_period)
+        # self.get_logger().info('time elapsed (s): "%s"'% time_elapsed)
+        if self.count == 0:
+            self.prev_angle_l = angle_l
+            self.prev_angle_r = angle_r
         
-        self.distance_1 += (self.delta_angle / 7) * (3.14 / 180) * 4 * 0.0254
-        self.get_logger().info('distance (1): "%s"' % self.distance_1)
-        self.prev_angle = angle
+        self.distance_l,self.prev_angle_l = self.get_distance(angle_l,self.prev_angle_l,self.distance_l)
+        self.distance_r,self.prev_angle_r = self.get_distance(angle_r,self.prev_angle_r,self.distance_r)
+        
+        self.vel_l,self.prev_distance_l = self.get_velocity(self.distance_l,self.prev_distance_l,timer_period)
+        self.vel_r,self.prev_distance_r = self.get_velocity(self.distance_r,self.prev_distance_r,timer_period)
+
+        self.get_logger().info('distance (l) (m): "%s"' % self.distance_l)
+        self.get_logger().info('velocity (l) (m/s): "%s"' % self.vel_l)
+        self.get_logger().info('distance (r) (m): "%s"' % self.distance_r)
+        self.get_logger().info('velocity (r) (m/s): "%s"' % self.vel_r)
+        
+
+        self.count += 1
+    def listener_callback2(self,msg):
+        acc_x = float(msg.data[0])
+        acc_y = float(msg.data[1])
+        acc_z = float(msg.data[2])
+        gyr_x = float(msg.data[3])
+        gyr_y = float(msg.data[4])
+        gyr_z = float(msg.data[5])
+        timer_period = float(msg.data[6])
+        self.get_logger().info('acc_x : "%s" m/s^2 acc_y: "%s" m/s^2 acc_z: "%s" m/s^2' % (acc_x,acc_y,acc_z))
+        self.get_logger().info('gyr_x : "%s" rad/s gyr_y: "%s" rad/s gyr_z: "%s" rad/s' % (gyr_x,gyr_y,gyr_z))
+        #find some way to get acc_x,acc_y,acc_z into actual acc_x,acc_y,acc_z
+        # self.vel_x,self.dist_x = self.integrate(acc_x,self.vel_x,self.dist_x,timer_period)
+        # self.vel_y,self.dist_y = self.integrate(acc_y,self.vel_y,self.dist_y,timer_period)
+        # self.vel_z,self.dist_z = self.integrate(acc_z,self.vel_z,self.dist_z,timer_period)
+
+
+    def get_distance(self,angle,prev_angle, distance):
+        delta_angle = 0
+        if prev_angle > 270 and angle < 90:
+            delta_angle = angle + 360 - prev_angle
+        elif prev_angle < 90 and angle > 270:
+            delta_angle = 360 - angle + prev_angle
+        else:
+            delta_angle = angle - prev_angle
+        distance += (delta_angle/7) * (math.pi/180)*4*0.0254
+        prev_angle = angle
+        return distance, prev_angle
+    def get_velocity(self,dist,prev_dist,timer_period):
+        vel = (dist - prev_dist)/timer_period
+        prev_dist = dist
+        return vel, prev_dist
+    def integrate(self,acc,vel,dist,timer_period):
+        vel = vel + acc * timer_period
+        dist = dist + vel * timer_period
+        return vel, dist
+        
+
+        
+            
 
 
 def main(args=None):
