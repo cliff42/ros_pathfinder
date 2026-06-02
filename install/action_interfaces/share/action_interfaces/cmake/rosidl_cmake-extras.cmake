0afbac3afef5c095f89f1a58742df3c8535a523e
chris cliff <cliff42@students.cs.ubc.ca>
2026-06-01 20:50:44 -0600

    untrack

--- install/action_interfaces/share/action_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(action_interfaces_IDL_FILES "action/MotorControl.idl;action/FollowPath.idl")
-set(action_interfaces_INTERFACE_FILES "action/MotorControl.action;action/FollowPath.action")
